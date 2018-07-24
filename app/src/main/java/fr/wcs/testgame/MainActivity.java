a821ed783ff653d6131e65ddf2c357d4703dce58
WOLFF Benjamin <wolfbenjamin31@gmail.com>
2018-07-24 11:17:55 +0200

    m

---
+++ app/src/main/java/fr/wcs/testgame/MainActivity.java
@@ -0,0 +1,26 @@
+package fr.wcs.testgame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonPlay = findViewById(R.id.button_play);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ActionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
